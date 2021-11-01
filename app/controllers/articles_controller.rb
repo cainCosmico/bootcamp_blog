f9d938bce7b5800648f6097f468f6e51487bd276
Camilo Valencia <acvalencia.kommit@gmail.com>
2021-11-01 15:35:45 -0500

    implemented confirmation email

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,4 @@
 class ArticlesController < ApplicationController
-
-  # http_basic_authenticate_with name: "dhh", password: "secret", 
-  #                              except: [:index, :show]
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,7 +72,19 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
-
+  # config.action_cable.disable_request_forgery_protection = true  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:        "smtp.gmail.com",
+    port:           465,
+    domain:         "gmail.com",
+    user_name:      ENV["SENDMAIL_USERNAME"],
+    password:       ENV["SENDMAIL_PASSWORD"],
+    authentication: :plain,
+    ssl: true,
+    tsl: true,
+    enable_starttls_auto: true
+  }
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end

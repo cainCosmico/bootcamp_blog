4b9703e7adb71ec2de817e9b88d18917d0470f90
Camilo Valencia <acvalencia.kommit@gmail.com>
2021-11-15 21:31:37 -0500

    changed the user model, now uses one fk for each follower, and followed

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,15 +7,13 @@ class UserController < ApplicationController
   end
 
   def follow
-    @user = User.find(params[:id])
-    current_user.followeds << @user
-    redirect_to root_path
+    current_user.followeds.create(follower_id: current_user.id, followed_id: params[:id])
+    redirect_back(fallback_location: root_path)
   end
   
   def unfollow
-    @user = User.find(params[:id])
-    current_user.followed_users.find_by(followed_id: @user.id).destroy
-    redirect_to root_path
+    current_user.followeds.find_by(followed_id: params[:id]).destroy
+    redirect_back(fallback_location: root_path)
   end
 
   def followers
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
-
-  has_many :followed_users, foreign_key: :follower_id, class_name: 'Follow'
-  has_many :followeds, through: :followed_users
-
-  has_many :following_users, foreign_key: :followed_id, class_name: 'Follow'
-  has_many :followers, through: :following_users
-
+  
+  has_many :followeds, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :followers, foreign_key: :followed_id, class_name: 'Follow'
+  
   has_many :articles
 end
